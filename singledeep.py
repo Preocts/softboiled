c80e88e78e5c3fb35d03d523a5af689a8d55f8e0
Preocts <preocts@gmail.com>
2021-08-12 22:21:30 -0400

    Resolve failed test on missing parameter
    If we are missing a required parameter then create that key/value pair
    in the `data` dict with a default of `None`. Check that the expected
    field is `Optional` and output a warnning if it is not.
    
    This allows us to build the dataclass with too few parameters.

--- singledeep.py
+++ singledeep.py
@@ -5,6 +5,7 @@ filters its own parameters on init
 from __future__ import annotations
 
 import dataclasses
+import logging
 from typing import Any
 from typing import Dict
 from typing import Optional
@@ -12,17 +13,45 @@ from typing import Type
 
 
 class SafeLoader:
+    log = logging.getLogger("SafeLoader")
+
     @staticmethod
     def cleandata(obj: Type, data: Dict[str, Any]) -> Dict[str, Any]:
         """Cleans data, removing keys that are not supported by object"""
+
+        fulldata = SafeLoader.__addmissing(obj, data)
         expected = [field.name for field in dataclasses.fields(obj)]
-        return {key: value for key, value in data.items() if key in expected}
+
+        return {key: value for key, value in fulldata.items() if key in expected}
+
+    @staticmethod
+    def __addmissing(obj: Type, data: Dict[str, Any]) -> Dict[str, Any]:
+        """Adds missing key/values as None. Warns if not optional"""
+
+        return_data: Dict[str, Any] = {}
+
+        for field in dataclasses.fields(obj):
+
+            if field.name not in data:
+                return_data.update({field.name: None})
+
+                if "Optional" not in field.type:
+                    SafeLoader.log.warning(
+                        "Type Warning: required key missing, now None '%s'", field.name
+                    )
+
+            else:
+
+                return_data.update({field.name: data[field.name]})
+
+        return return_data
 
 
 @dataclasses.dataclass
 class SimpleLayer(SafeLoader):
     data01: str
-    data02: Optional[str]
+    data02: str
+    data03: Optional[str]
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> SimpleLayer:
--- singledeep_test.py
+++ singledeep_test.py
@@ -11,25 +11,41 @@ TOO_SMALL = {
 JUST_RIGHT = {
     "data01": "This is all",
     "data02": "There is no more",
+    "data03": "Why are you still here",
 }
 
 TOO_MUCH = {
     "data01": "This is all",
     "data02": "There is no more",
     "data03": "Why are you still here",
+    "data04": "Go home",
 }
 
 
-def test_too_small() -> None:
+def test_too_small(caplog) -> None:
     """Pass/fail"""
-    SimpleLayer.from_dict(TOO_SMALL)
+    result = SimpleLayer.from_dict(TOO_SMALL)
+
+    assert result.data01 == TOO_SMALL["data01"]
+    assert result.data02 is None
+    assert result.data03 is None
+
+    assert "Type Warning: required key missing" in caplog.text
 
 
 def test_just_right() -> None:
     """Pass/fail"""
-    SimpleLayer.from_dict(JUST_RIGHT)
+    result = SimpleLayer.from_dict(JUST_RIGHT)
+
+    assert result.data01 == JUST_RIGHT["data01"]
+    assert result.data02 == JUST_RIGHT["data02"]
+    assert result.data03 == JUST_RIGHT["data03"]
 
 
 def test_too_large() -> None:
     """Pass/fail"""
-    SimpleLayer.from_dict(TOO_MUCH)
+    result = SimpleLayer.from_dict(TOO_MUCH)
+
+    assert result.data01 == JUST_RIGHT["data01"]
+    assert result.data02 == JUST_RIGHT["data02"]
+    assert result.data03 == JUST_RIGHT["data03"]
